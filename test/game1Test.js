8848e3116262eb6e515cdf868637e51620928ea4
alice3773 <aliceyiu0307@gmail.com>
2022-06-05 19:25:32 +0800

    finished classroom activities

--- test/game1Test.js
+++ test/game1Test.js
@@ -1,16 +1,17 @@
 const { assert } = require("chai");
 
 describe("Game1", function() {
-  it("should be a winner", async function() {
-    const Game = await ethers.getContractFactory("Game1");
-    const game = await Game.deploy();
-    await game.deployed();
+    it("should be a winner", async function() {
+        const Game = await ethers.getContractFactory("Game1");
+        const game = await Game.deploy();
+        await game.deployed();
 
-    // you must call unlock before you can win
+        // you must call unlock before you can win
+        await game.unlock();
 
-    await game.win();
+        await game.win();
 
-    // leave this assertion as-is
-    assert(await game.isWon(), "You did not win the game");
-  });
+        // leave this assertion as-is
+        assert(await game.isWon(), "You did not win the game");
+    });
 });
--- test/game2Test.js
+++ test/game2Test.js
@@ -1,16 +1,19 @@
 const { assert } = require("chai");
 
 describe("Game2", function() {
-  it("should be a winner", async function() {
-    const Game = await ethers.getContractFactory("Game2");
-    const game = await Game.deploy();
-    await game.deployed();
+    it("should be a winner", async function() {
+        const Game = await ethers.getContractFactory("Game2");
+        const game = await Game.deploy();
+        await game.deployed();
 
-    // press all the right switches to win this stage
+        // press all the right switches to win this stage
+        game.switchOn(20);
+        game.switchOn(47);
+        game.switchOn(212);
 
-    await game.win();
+        await game.win();
 
-    // leave this assertion as-is
-    assert(await game.isWon(), "You did not win the game");
-  });
+        // leave this assertion as-is
+        assert(await game.isWon(), "You did not win the game");
+    });
 });
