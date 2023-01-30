61c531fb98e99ca0ab74ab6954d1187a80f2481f
Richard Hao <richard@digitalstar.tech>
2023-01-30 18:59:07 +0800

    feat: solve game 3

--- test/game3Test.js
+++ test/game3Test.js
@@ -10,24 +10,24 @@ describe('Game3', function () {
     // you can get one of this accounts with ethers.provider.getSigner
     // and passing in the zero-based indexed of the signer you want:
     const signer = ethers.provider.getSigner(0);
+    const signer1 = ethers.provider.getSigner(1);
+    const signer2 = ethers.provider.getSigner(2);
 
-    // you can get that signer's address via .getAddress()
-    // this variable is NOT used for Contract 3, just here as an example
-    const address = await signer.getAddress();
-
-    return { game, signer };
+    return { game, signer, signer1, signer2 };
   }
 
   it('should be a winner', async function () {
-    const { game, signer } = await loadFixture(deployContractAndSetVariables);
+    const { game, signer, signer1, signer2 } = await loadFixture(deployContractAndSetVariables);
 
     // you'll need to update the `balances` mapping to win this stage
 
     // to call a contract as a signer you can use contract.connect
-    await game.connect(signer).buy({ value: '1' });
+    await game.connect(signer).buy({ value: '10' });
+    await game.connect(signer1).buy({ value: '20' });
+    await game.connect(signer2).buy({ value: '5' });
 
-    // TODO: win expects three arguments
-    await game.win();
+    // win expects three arguments
+    await game.win(signer.getAddress(), signer1.getAddress(), signer2.getAddress());
 
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
