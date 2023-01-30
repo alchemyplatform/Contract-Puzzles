572767ec767599cc89ad11c5d5b25239d499440a
Richard Hao <richard@digitalstar.tech>
2023-01-30 21:18:27 +0800

    feat: solve Game5

--- contracts/Contract5.sol
+++ contracts/Contract5.sol
@@ -2,13 +2,13 @@
 pragma solidity 0.8.17;
 
 contract Game5 {
-  bool public isWon;
+    bool public isWon;
 
-  address threshold = 0x00FfFFfFFFfFFFFFfFfFfffFFFfffFfFffFfFFFf;
+    address threshold = 0x00FfFFfFFFfFFFFFfFfFfffFFFfffFfFffFfFFFf;
 
-  function win() external {
-    require(bytes20(msg.sender) < bytes20(threshold), "Nope. Try again!");
+    function win() external {
+        require(bytes20(msg.sender) < bytes20(threshold), "Nope. Try again!");
 
-    isWon = true;
-  }
+        isWon = true;
+    }
 }
--- test/game5Test.js
+++ test/game5Test.js
@@ -1,5 +1,6 @@
 const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
 const { assert } = require('chai');
+const { ethers } = require('hardhat');
 
 describe('Game5', function () {
   async function deployContractAndSetVariables() {
@@ -8,12 +9,32 @@ describe('Game5', function () {
 
     return { game };
   }
+
+  const getEligibleWallet = async () => {
+    const threshold = 0x00FfFFfFFFfFFFFFfFfFfffFFFfffFfFffFfFFFf;
+    let eligibleWallet;
+    while(true) {
+      let wallet = ethers.Wallet.createRandom();
+      if(wallet.address < threshold) {
+        eligibleWallet = wallet
+        eligibleWallet = wallet.connect(ethers.provider);
+        break
+      }
+    }
+
+    const [owner] = await ethers.getSigners()
+    await owner.sendTransaction({to: eligibleWallet.address, value: ethers.utils.parseEther("1")});
+
+    return eligibleWallet;
+  };
+
   it('should be a winner', async function () {
+    const wallet = await getEligibleWallet();
     const { game } = await loadFixture(deployContractAndSetVariables);
 
     // good luck
 
-    await game.win();
+    await game.connect(wallet).win();
 
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
