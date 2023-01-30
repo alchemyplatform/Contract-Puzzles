cf29bf4f38851b02587a58f42faa28c8e16ec37b
Richard Hao <richard@digitalstar.tech>
2023-01-30 16:51:43 +0800

    feat: solve Game2

--- test/game2Test.js
+++ test/game2Test.js
@@ -13,6 +13,9 @@ describe('Game2', function () {
     const { game } = await loadFixture(deployContractAndSetVariables);
 
     // press all the right switches to win this stage
+    await game.switchOn(20);
+    await game.switchOn(47);
+    await game.switchOn(212);
 
     await game.win();
 
