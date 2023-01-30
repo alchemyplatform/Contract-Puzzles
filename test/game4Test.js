8489f45b10755ab8c301b03751274242e784b79d
Richard Hao <richard@digitalstar.tech>
2023-01-30 19:08:34 +0800

    feat: resolve game4

--- test/game4Test.js
+++ test/game4Test.js
@@ -3,17 +3,19 @@ const { assert } = require('chai');
 
 describe('Game4', function () {
   async function deployContractAndSetVariables() {
+    const [owner, otherAccount] = await ethers.getSigners();
     const Game = await ethers.getContractFactory('Game4');
     const game = await Game.deploy();
 
-    return { game };
+    return { game, owner, otherAccount };
   }
   it('should be a winner', async function () {
-    const { game } = await loadFixture(deployContractAndSetVariables);
+    const { game, owner, otherAccount } = await loadFixture(deployContractAndSetVariables);
 
     // nested mappings are rough :}
+    await game.connect(otherAccount).write(owner.getAddress())
 
-    await game.win();
+    await game.win(otherAccount.getAddress());
 
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
