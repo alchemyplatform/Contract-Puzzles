129a3b66cfd397d7c23147a724428b4cd231173a
Richard Hao <richard@digitalstar.tech>
2023-01-30 16:43:12 +0800

    feat: solve game1

--- test/game1Test.js
+++ test/game1Test.js
@@ -14,6 +14,7 @@ describe('Game1', function () {
     const { game } = await loadFixture(deployContractAndSetVariables);
 
     // you must call unlock before you can win
+    await game.unlock();
 
     // leave this call to game.win() as-is
     await game.win();
