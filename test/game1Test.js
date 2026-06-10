da0d99c3e3dcac19de6822a291486c456d389611
Priyanshu Rajput <priyanshurajput360@gmail.com>
2026-06-10 13:13:32 +0530

    solved 1st test

--- test/game1Test.js
+++ test/game1Test.js
@@ -1,24 +1,25 @@
-const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
-const { assert } = require('chai');
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+const { assert } = require("chai");
 
-describe('Game1', function () {
+describe("Game1", function () {
   async function deployContractAndSetVariables() {
-    const Game = await ethers.getContractFactory('Game1');
+    const Game = await ethers.getContractFactory("Game1");
     const game = await Game.deploy();
 
     return { game };
   }
 
-  it('should be a winner', async function () {
+  it("should be a winner", async function () {
     // leave this as-is
     const { game } = await loadFixture(deployContractAndSetVariables);
 
     // you must call unlock before you can win
+    await game.unlock();
 
     // leave this call to game.win() as-is
     await game.win();
 
     // leave this testing assertion as-is
-    assert(await game.isWon(), 'You did not win the game');
+    assert(await game.isWon(), "You did not win the game");
   });
 });
